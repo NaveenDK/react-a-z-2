f74631212864434993c840f0c6d8547eb2f96000
NaveenDK <naveenkolambage@gmail.com>
2018-02-16 01:44:15 +1300

    added another show-hide-refactored file simpler version

---
+++ src/playground/show-hide-refactored.js
@@ -0,0 +1,10 @@
+let visibility = false;
+
+const render = () =>{
+    const template = (
+        <div>
+            <h1>Visibility Toggle</h1>
+        <button>    {visibility ? 'Hide Details':'Show Details'} </button>
+            </div>
+    )
+}
