08ad474b2175a9ea8559270ca481b5c663727807
NaveenDK <naveenkolambage@gmail.com>
2018-02-16 01:53:18 +1300

    the simpler version added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,51 +1,38 @@
-"use strict";
+'use strict';
 
-var hideLine = {
-    line: 'test--test--test',
-    hidden: true
+var visibility = false;
+
+var toggleVisibility = function toggleVisibility() {
+    visibility = !visibility;
+    renderTemplate();
 };
-var namedetails = "Show";
-var ShowDetails = function ShowDetails() {
-    console.log("Test this");
-    if (hideLine.hidden == true) {
-        hideLine.hidden = false;
-        namedetails = "Hide";
-    } else if (hideLine.hidden == false) {
-        hideLine.hidden = true;
-        namedetails = "Show";
-    }
-
-    RenderTemplate();
-};
-
-var getLine = function getLine() {
-    if (hideLine.hidden === false) {
-        return React.createElement(
-            "p",
-            null,
-            "This is to be shown Click Again to Hide this Element "
-        );
-    } else {
-        return React.createElement("p", null);
-    }
-};
-
-var RenderTemplate = function RenderTemplate() {
+var renderTemplate = function renderTemplate() {
     var template = React.createElement(
-        "div",
+        'div',
         null,
         React.createElement(
-            "button",
-            { onClick: ShowDetails },
-            " ",
-            namedetails,
-            " Details"
+            'h1',
+            null,
+            'Visibility Toggle'
         ),
-        getLine()
+        React.createElement(
+            'button',
+            { onClick: toggleVisibility },
+            '    ',
+            visibility ? 'Hide Details' : 'Show Details',
+            ' '
+        ),
+        React.createElement(
+            'div',
+            null,
+            React.createElement(
+                'p',
+                null,
+                visibility ? 'These are the details that toggles off and on' : '',
+                ' '
+            )
+        )
     );
-    var appRoot = document.getElementById('app');
-    ReactDOM.render(template, appRoot);
+    ReactDOM.render(template, document.getElementById('app'));
 };
-
-RenderTemplate();
-//React.DOM.render()
+renderTemplate();
--- src/playground/show-hide-refactored.js
+++ src/playground/show-hide-refactored.js
@@ -1,10 +1,19 @@
-let visibility = false;
+let visibility = false
 
-const render = () =>{
+const toggleVisibility = ()=> {
+    visibility = !visibility
+    renderTemplate();
+}
+const renderTemplate = () =>{
     const template = (
         <div>
             <h1>Visibility Toggle</h1>
-        <button>    {visibility ? 'Hide Details':'Show Details'} </button>
+        <button onClick={toggleVisibility}>    {visibility ? 'Hide Details':'Show Details'} </button>
+        <div>
+            <p>{visibility?'These are the details that toggles off and on': ''} </p>
+        </div>
             </div>
     )
+    ReactDOM.render(template,document.getElementById('app'));
 }
+renderTemplate();
