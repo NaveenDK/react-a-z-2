6553a7831b91babfb1857715404691110c49b58c
NaveenDK <naveenkolambage@gmail.com>
2018-02-18 02:26:56 +1300

    added Student,Traveller to Person -playground files

--- src/playground/es6-classes.js
+++ src/playground/es6-classes.js
@@ -7,13 +7,53 @@ class Person {
         return `Hi , I am ${this.name}!`
     }
     getDescription(){
-        return `${this.name} is ${this.age} years old`
+        return `${this.name} is ${this.age} years old. `
     }
 }
 
-const me = new Person('Andrew Mead',4);
+
+class Student extends Person {
+    //add support for major    
+    constructor(name,age,major){
+        super(name,age)
+        this.major =major
+    }
+    hasMajor(){
+        return !!this.major
+    }
+    getDescription(){
+        
+        let description = super.getDescription();
+
+        if (this.hasMajor()){
+            description+= `The major is ${this.major} `;
+        }
+        return description;
+    }
+    
+}
+class Traveller extends Person{
+    constructor(name,age,homeLocation){
+        super(name,age)
+
+        this.homeLocation=homeLocation;
+    }
+    hasHome(){
+        return !!this.homeLocation;
+    }
+    getGreeting(){
+        let greeting = super.getGreeting();
+
+        if(this.hasHome()){
+            greeting += ` Im visiting from ${this.homeLocation}` ;
+        }
+        return greeting;
+    }
+}
+const me = new Traveller('Andrew Mead',4,'America');
 console.log(me.getGreeting());
 console.log(me.getDescription());
+console.log(me);
 
 const other = new Person();
 console.log(other.getGreeting());
